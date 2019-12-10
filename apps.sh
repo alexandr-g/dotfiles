9f4258f3ecf4fc355a6c609bf99d39ba12db0593
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2019-12-10 12:31:29 +0100

    Add list of apps

---
+++ apps.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Install nvm (https://github.com/nvm-sh/nvm)
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+
+# Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Set zsh as default shell
+chsh -s $(which zsh)
+
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# Spectacle
+brew cask install spectacle 
+
+# iTerm2
+brew cask install iTerm2
+
+# Spotify
+brew cask install spotify
+
+# Visual Studio Code
+brew cask install visual-studio-code
+
+# Docker
+brew cask install docker
+
+# Dropbox
+brew cask install dropbox
+
+# Chrome
+brew cask install google-chrome
+
+# Firefox
+brew cast install firefox
+
+# Alfred
+brew cask install alfred
+
+# KeePassX
+brew cask install keepassx
+
+# gitx
+brew cask install rowanj-gitx
+
+# Slack
+brew cask install slack
