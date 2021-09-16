16ae279ef6819fd194eb971653efb187e3bd48cd
Alexander Grischuk <alexgrischuk@gmail.com>
2021-09-16 23:25:56 +0200

    update apps install script

--- apps.sh
+++ apps.sh
@@ -12,46 +12,19 @@ chsh -s $(which zsh)
 # Install zsh-autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-
-brew tap homebrew/cask-cask
-brew tap homebrew/versions
 brew tap homebrew/cask-fonts
 
-# Fira code font
-brew cask install font-fira-code
-
-# Spectacle
-brew cask install spectacle 
-
-# iTerm2
-brew cask install iTerm2
-
-# Spotify
-brew cask install spotify
-
-# Visual Studio Code
-brew cask install visual-studio-code
-
-# Docker
-brew cask install docker
-
-# Dropbox
-brew cask install dropbox
-
-# Chrome
-brew cask install google-chrome
-
-# Firefox
-brew cast install firefox
-
-# Alfred
-brew cask install alfred
-
-# KeePassX
-brew cask install keepassx
-
-# gitx
-brew cask install rowanj-gitx
-
-# Slack
-brew cask install slack
+brew install --cask \
+    font-fira-code \
+    spectacle \
+    iTerm2 \
+    spotify \
+    visual-studio-code \
+    docker \
+    dropbox \
+    google-chrome \
+    firefox \
+    alfred \
+    keepassx \
+    rowanj-gitx \
+    slack
